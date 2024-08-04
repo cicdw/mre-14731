6a205058f431fbcff000d4b74aa4f2deabafee9b
Chris White <chris@prefect.io>
2024-08-04 15:31:45 -0500

    fix typo

--- test.py
+++ test.py
@@ -1,7 +1,11 @@
 from prefect import flow, task
-from my_14731_sub.blocktypes.test_block import TestBlock
+from mre_14731_sub.blocktypes.test_block import TestBlock
 
 
 @flow(log_prints=True)
 def process_function():
     print("I'm here")
+
+
+if __name__ == '__main__':
+    process_function()
